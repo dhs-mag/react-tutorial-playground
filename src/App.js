649051c458576f556e4e955865a976f66c99e1cb
Ondrej Doktor <doktor.ml@gmail.com>
2019-04-01 13:17:30 +0200

    LEKCE 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Player from "./components/Player";
+import PlayerForm from "./components/PlayerForm";
+
+const styles = {
+    container: {
+        display: 'flex',
+        padding: '2em',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: "center"
+    },
+};
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-          <Player/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div style={styles.container}>
+                <PlayerForm/>
+                <Player/>
+            </div>
+        );
+    }
 }
 
 export default App;
